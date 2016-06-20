ebe5cb62b22986a7743a4f91691bad0abb430fbd
Xiaolei Zhu <childmercy@gmail.com>
2016-06-20 22:03:41 +0800

    Initial Commit

---
+++ OpenglRedBookExample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenglRedBookExample
+//
+//  Created by ZhuXiaolei on 6/20/16.
+//  Copyright © 2016 ZhuXiaolei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
