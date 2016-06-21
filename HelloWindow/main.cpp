cec5c24de524dc7fe2ebf906110010f8bb0fc1ea
ZhuXiaolei <xiaolei@ZhuXiaoleide-MacBook-Pro.local>
2016-06-21 22:50:15 +0800

    add new target

---
+++ HelloWindow/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  HelloWindow
+//
+//  Created by ZhuXiaolei on 6/21/16.
+//  Copyright © 2016 ZhuXiaolei. All rights reserved.
+//
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
